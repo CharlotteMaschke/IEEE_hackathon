5ba42d8cde6d92c3e850ecfd52d76d6f32e55680
chrisini <christina.bornberg@outlook.com>
2020-10-11 21:42:40 +0200

    created the get to know your data part

--- load_data.py
+++ load_data.py
@@ -22,3 +22,27 @@ dataP1h2 = scipy.io.loadmat('data/P1_high2.mat')
 trig = pd.DataFrame(dataP1h2['trig'])
 y = pd.DataFrame(dataP1h2['y'])
 
+def analyse_data(trig, y):
+    ''' can be moved to other file '''
+    # get head
+    print('******* TRIG HEAD *******')
+    print(trig.head())
+    print('******* Y HEAD *******')
+    print(y.head())
+    
+    # get info
+    print('******* TRIG INFO *******')
+    print(trig.info())
+    print('******* Y INFO *******')
+    print(y.info())
+    
+    # describe
+    print('******* TRIG DESCRIBE *******')
+    print(trig.describe())
+    print('******* Y DESCRIBE *******')
+    print(y.describe())
+    
+    print('******* TRIG VALUES (0, 1, 2, -1 *******')
+    print(trig[0].value_counts())
+    
+analyse_data(trig, y) # call or not call
