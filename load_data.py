3d4a56d876677fa8ce9bf216b932630f13f51634
CharlotteMaschke <55599742+CharlotteMaschke@users.noreply.github.com>
2020-10-11 14:14:09 -0400

    add_dataloader_part1

---
+++ load_data.py
@@ -0,0 +1,11 @@
+import scipy.io
+
+# load Data
+dataP1h2 = scipy.io.loadmat('data/P1_high2.mat')
+dataP1l1 = scipy.io.loadmat('data/P1_low1.mat')
+dataP1l2 = scipy.io.loadmat('data/P1_low2.mat')
+
+dataP2h1 = scipy.io.loadmat('data/P2_high1.mat')
+dataP2h2 = scipy.io.loadmat('data/P2_high2.mat')
+dataP2l2 = scipy.io.loadmat('data/P2_low2.mat')
+
